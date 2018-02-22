ca322e3056b0124ea190f33eb60adb4d8c940ff1
Eva Gyomrey <evamargy@gmail.com>
2018-02-22 08:56:13 -0400

    Add new president: Ramon Velasquez. Modify the Hugo Chavez card by showing all its periods. Change the back of the cards.

--- src/js/app.js
+++ src/js/app.js
@@ -83,39 +83,51 @@ var cards = [{
     period: "1989-1993",
     info: "Electo presidente popularmente. Contexto de bajos precios del petróleo y escasas reservas internacionales. En febrero se establecen las coordenadas del nuevo modelo económico con el cual se busca revitalizar la economía del país y dejar atrás el esquema ISI. El día 27 de febrero sucede el Caracazo, una revuelta popular que se extiende a otras ciudades del país. La causa: un aumento del pasaje ilegal por parte de los transportistas. Para el periodo 1990-1991 hay crecimiento de la economía, aumento de las reservas internacionales, pago de la deuda externa y descenso en los índices de inflación. En febrero de 1992 Hugo Chávez y otros oficiales dan un golpe de Estado fallido. Seguidamente en noviembre ocurre otro golpe de estado fallido esta vez realizado por Francisco Visconti Osorio."
 }, {
+    name: "Ramón José Velasquez",
+    img: "./img/J-Ramon-Velasquez.jpg",
+    id: 15,
+    period: "1994-1999",
+    info: "Congreso lo elige presidente de la república para finalizar ese periodo presidencial. El Congreso le concede una Ley Habilitante y de ese modo en noviembre se aprueba el IVA. Impulsó la descentralización creando, entre otras medidas, el ministerio de Estado para la Descentralización. En enero de 1994 el Banco Latino quiebra y es intervenido por el Estado. Se genera una crisis en el sistema financiero. El sistema de partidos políticos empieza a colapsar. Nuevas fuerzas políticas empiezan a surgir."
+}, {
+    name: "Ramón José Velasquez",
+    img: "./img/F-Ramon-Velasquez.jpg",
+    id: 16,
+    period: "1994-1999",
+    info: "Congreso lo elige presidente de la república para finalizar ese periodo presidencial. El Congreso le concede una Ley Habilitante y de ese modo en noviembre se aprueba el IVA. Impulsó la descentralización creando, entre otras medidas, el ministerio de Estado para la Descentralización. En enero de 1994 el Banco Latino quiebra y es intervenido por el Estado. Se genera una crisis en el sistema financiero. El sistema de partidos políticos empieza a colapsar. Nuevas fuerzas políticas empiezan a surgir."
+}, {
     name: "Rafael Caldera",
     img: "./img/J-Rafael-Caldera2.jpg",
-    id: 15,
+    id: 17,
     period: "1994-1999",
     info: "Electo popularmente. Entre 1994 y 1995 se agrava la crisis financiera y varios bancos son intervenidos. Se crea el SENIAT en un contexto de precios del petróleo bajos y estables. En 1994 Caldera sobreseyó a los militares insurrectos de 1992, dejando en libertad, entre otros a Hugo Chávez. En lo económico se aplica la llamada Agenda Venezuela, un programa orientado hacia la apertura petrolera, la atracción de nuevas inversiones al país y control del déficit fiscal, entre otras medidas. La anti política y la crítica a los partidos en su apogeo"
 }, {
     name: "Rafael Caldera",
     img: "./img/F-Rafael-Caldera2.jpg",
-    id: 16,
+    id: 18,
     period: "1994-1999",
     info: "Electo popularmente. Entre 1994 y 1995 se agrava la crisis financiera y varios bancos son intervenidos. Se crea el SENIAT en un contexto de precios del petróleo bajos y estables. En 1994 Caldera sobreseyó a los militares insurrectos de 1992, dejando en libertad, entre otros a Hugo Chávez. En lo económico se aplica la llamada Agenda Venezuela, un programa orientado hacia la apertura petrolera, la atracción de nuevas inversiones al país y control del déficit fiscal, entre otras medidas. La anti política y la crítica a los partidos en su apogeo"
 }, {
     name: "Hugo Chávez",
     img: "./img/J-Hugo-Chavez.jpg",
-    id: 17,
+    id: 19,
     period: "1999-2013",
     info: "Electo popularmente.En abril se da un referéndum consultivo para llamar a una Asamblea Constituyente y refundar el Estado. La propuesta triunfa. En Julio se realiza la elección para dicha Asamblea Constituyente obteniendo los miembros del MVR la casi mayoría total de curules. En diciembre cesan las labores del antiguo Congreso Nacional. Ese mismo mes, después del referéndum de aprobación, se promulga la Constitución de 1999. Periodo presidencial ampliado a 6 años con reelección inmediata. Se introduce el Referéndum Revocatorio, se introducen dos nuevos poderes y se restablece el cargo de Vicepresidente, inexistente en la de 1961. De enero a agosto de 2000 la Asamblea Nacional funciona de forma parecida a una comisión delegada con pocos diputados, que se encargarán de continuar con los asuntos legislativos del país y que llamarán popularmente como el “Congresillo”. Según la nueva Constitución se llama nuevamente a elecciones generales. FALTA INFORMACION!!!!"
 }, {
     name: "Hugo Chávez",
     img: "./img/F-Hugo-Chavez.jpg",
-    id: 18,
+    id: 20,
     period: "1999-2013",
     info: "Electo popularmente.En abril se da un referéndum consultivo para llamar a una Asamblea Constituyente y refundar el Estado. La propuesta triunfa. En Julio se realiza la elección para dicha Asamblea Constituyente obteniendo los miembros del MVR la casi mayoría total de curules. En diciembre cesan las labores del antiguo Congreso Nacional. Ese mismo mes, después del referéndum de aprobación, se promulga la Constitución de 1999. Periodo presidencial ampliado a 6 años con reelección inmediata. Se introduce el Referéndum Revocatorio, se introducen dos nuevos poderes y se restablece el cargo de Vicepresidente, inexistente en la de 1961. De enero a agosto de 2000 la Asamblea Nacional funciona de forma parecida a una comisión delegada con pocos diputados, que se encargarán de continuar con los asuntos legislativos del país y que llamarán popularmente como el “Congresillo”. Según la nueva Constitución se llama nuevamente a elecciones generales. FALTA INFORMACION!!!!"
 }, {
     name: "Nicolás Maduro",
     img: "./img/J-Nicolas-Maduro.jpg",
-    id: 19,
+    id: 21,
     period: "2013-",
     info: "Electo popularmente para completar el período de su predecesor. Presenta el Plan de la Patria (plan propuesto por Chávez en las elecciones de 2012) como hoja de ruta de la nación hacia el Socialismo. Los problemas de escasez de productos, alta inflación e inseguridad crecen. En febrero de 2014 se desatan en distintos estados del país protestas pacíficas contra los grandes problemas del país protagonizadas por estudiantes universitarios. Durante marzo y abril continúan las manifestaciones y en algunas regiones del país se tornan violentas con detenciones y abusos por parte de las FFAA y actos vandálicos por parte de algunos grupos. Mueren varios manifestantes en el transcurso de estos sucesos. En abril una comisión de la UNASUR en conjunto con el Vaticano se ofrecen como mediadores para un diálogo entre la oposición política y el gobierno. Por su parte los estudiantes continúan en las calles siguiendo su propia agenda de protesta contra el gobierno."
 }, {
     name: "Nicolás Maduro",
     img: "./img/F-Nicolas-Maduro.jpg",
-    id: 20,
+    id: 22,
     period: "2013-",
     info: "Electo popularmente para completar el período de su predecesor. Presenta el Plan de la Patria (plan propuesto por Chávez en las elecciones de 2012) como hoja de ruta de la nación hacia el Socialismo. Los problemas de escasez de productos, alta inflación e inseguridad crecen. En febrero de 2014 se desatan en distintos estados del país protestas pacíficas contra los grandes problemas del país protagonizadas por estudiantes universitarios. Durante marzo y abril continúan las manifestaciones y en algunas regiones del país se tornan violentas con detenciones y abusos por parte de las FFAA y actos vandálicos por parte de algunos grupos. Mueren varios manifestantes en el transcurso de estos sucesos. En abril una comisión de la UNASUR en conjunto con el Vaticano se ofrecen como mediadores para un diálogo entre la oposición política y el gobierno. Por su parte los estudiantes continúan en las calles siguiendo su propia agenda de protesta contra el gobierno."
 }];
